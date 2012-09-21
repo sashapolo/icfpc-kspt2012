56265bcb272297778f259cd476fe45aa015969cf
yaromir <yaromir@hotbox.ru>
2012-09-21 17:18:13 +0400

    Created project. Correspondingly updated .gitignore.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: yaromir
+ *
+ * Created on September 21, 2012, 11:28 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
