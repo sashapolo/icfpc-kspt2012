c501e61c701b5e444f8da93e71b505de72bbc99f
KirillBel <kirill_bel@mail.ru>
2012-09-27 01:07:36 +0400

    Добавлены классы: Logger (суперкласс), HTMLLogger (дочерний класс, ведет лог в html документ)

---
+++ src/HTMLLogger.cpp
@@ -0,0 +1,94 @@
+#include "stdinclude.h"
+#include "HTMLLogger.h"
+
+
+HTMLLogger::HTMLLogger(void)
+{
+	bLogInit=false;
+}
+
+
+HTMLLogger::~HTMLLogger(void)
+{
+    Close();
+}
+
+void HTMLLogger::Init(char* FilePath, char* LogName)
+{
+	Close();
+
+	if(FilePath==NULL) LogFile.open("./LOG.html",std::ios::out | std::ios::trunc);
+	else LogFile.open(FilePath,std::ios::out | std::ios::trunc);
+
+	bLogInit=true;
+    if(LogName==NULL) StartBanner("Global Log");
+	else StartBanner(LogName);
+};
+
+void HTMLLogger::Close()
+{
+	if(bLogInit)
+	{
+		EndBanner();
+		LogFile.close();
+		bLogInit=false;
+	};
+};
+
+void HTMLLogger::OnMessageOut(LOGGER_MSG& Msg)
+{
+	std::string MsgType;
+
+        WriteTime(Msg.Time);
+	switch(Msg.MsgType)
+	{
+	case LOGGER_MSG_TYPE_INFO:	Write("#00FF00",Msg.String.c_str()); break;
+	case LOGGER_MSG_TYPE_WARNING:	Write("#FFFF00",Msg.String.c_str()); break;
+	case LOGGER_MSG_TYPE_ERROR:	Write("#FF0000",Msg.String.c_str()); break;
+	default:	Write("#FFFFFF",Msg.String.c_str()); break;
+	};
+}
+
+void  HTMLLogger::Write(const char* color, const char* string)
+{
+	if(!bLogInit) return;
+	if (!string) return;
+	
+    LogFile<<"<Font Size = 2 Color = "<<color
+            <<" Face = Verdana>"<<string<<"</font><BR>";
+	LogFile.flush();
+}
+
+void HTMLLogger::StartBanner(const char *logname)
+{
+	if(!bLogInit) return;
+	LogFile<<"<html>";
+    LogFile<<"<title>Log</title>";
+    LogFile<<"<meta http-equiv=\"Content-Type\"content=\"text/html; charset=windows-1251\">";
+    LogFile<<"<body bgcolor=\"#000000\" text=\"#aaaaaa\">";
+    LogFile<<"<pre>";
+    LogFile<<"<h1>Work Log - "<<logname<<"</h1><BR>";
+    LogFile<<"<h3><NoBR>----------------------------- Begin ------------------------------</NoBR></h3>";
+	LogFile.flush();
+}
+
+void HTMLLogger::EndBanner()
+{
+	if(!bLogInit) return;
+	LogFile<<"<h3><NoBR>---------------------------- Results -----------------------------</NoBR></h3>";
+	LogFile<<"<B>"<<GetNumInfos()<<" message(s), "<<GetNumErrors()<<" error(s), "<<GetNumWarnings()<<" warning(s)";
+    LogFile<<"</B></pre></body></html>";
+}
+
+void HTMLLogger::WriteTime(time_t time)
+{
+	if(!bLogInit) return;
+
+        tm * timeinfo;
+        timeinfo = localtime ( &time );
+
+    LogFile<<"<Font Size = 1 Color = #ABABAB Face = Verdana><B>";
+    LogFile<<timeinfo->tm_hour<<":"<<timeinfo->tm_min<<":"<<timeinfo->tm_sec<<" - ";
+    LogFile<<"</B></font>";
+	LogFile.flush();
+};
---
+++ src/Logger.cpp
@@ -0,0 +1,156 @@
+/* 
+ * File:   BaseLogger.cpp
+ * Author: kirill
+ * 
+ * Created on September 26, 2012, 6:02 PM
+ */
+
+#include "stdinclude.h"
+#include "Logger.h"
+
+
+//////////LOGGER EXTERNAL//////////////////////////////
+Logger* pCurrentLogger=0;
+
+void SetLogger(Logger* pLogger)
+{
+    pCurrentLogger=pLogger;
+};
+
+bool LogError(char* Msg,...)
+{
+    if(!pCurrentLogger) return false;
+    va_list args;
+    va_start(args, Msg);
+    pCurrentLogger->BaseOutV(LOGGER_MSG_TYPE_ERROR,Msg,args);
+    va_end(args);
+};
+
+bool LogWarning(char* Msg,...)
+{
+    if(!pCurrentLogger) return false;
+    va_list args;
+    va_start(args, Msg);
+    pCurrentLogger->BaseOutV(LOGGER_MSG_TYPE_WARNING,Msg,args);
+    va_end(args);
+};
+
+bool LogInfo(char* Msg,...)
+{
+    if(!pCurrentLogger) return false;
+    va_list args;
+    va_start(args, Msg);
+    pCurrentLogger->BaseOutV(LOGGER_MSG_TYPE_INFO,Msg,args);
+    va_end(args);
+};
+
+///////////////////////////////////////////////////////
+
+Logger::Logger() {
+    WarningsCount=0;
+    ErrorsCount=0;
+    MessagesCount=0;
+
+    MaxMessageCnt=512;
+}
+
+Logger::~Logger() {
+}
+
+void Logger::BaseOut(UINT MsgType,const char* Txt,...)
+{
+    pthread_mutex_lock(&CS2);
+    va_list args;
+    va_start(args, Txt);
+    BaseOutV(MsgType,Txt,args);
+    va_end(args);
+    pthread_mutex_unlock(&CS2);
+}
+
+void Logger::BaseOutV(UINT MsgType,const char* Txt,va_list args)
+{
+    if (!Txt) return;
+    pthread_mutex_lock(&CS1);
+
+    if(MsgType==LOGGER_MSG_TYPE_INFO) MessagesCount++;
+    else if(MsgType==LOGGER_MSG_TYPE_WARNING) WarningsCount++;
+    else if(MsgType==LOGGER_MSG_TYPE_ERROR) ErrorsCount++;
+
+    va_list arglen;
+    va_copy(arglen,args);
+    int len=vsnprintf(0,0,Txt, arglen)+ 1;
+    va_end(arglen);
+     
+    char* buffer = (char*)malloc( len * sizeof(char) );
+    vsnprintf(buffer, len, Txt, args);
+
+    LOGGER_MSG Msg;
+    time(&Msg.Time);
+    Msg.String=buffer;
+    Msg.MsgType=MsgType;
+
+    if((MaxMessageCnt==0) || (MsgList.size()<MaxMessageCnt)) MsgList.push_back(Msg);
+
+    free( buffer );
+    pthread_mutex_unlock(&CS1);
+
+    OnMessageOut(Msg);
+}
+
+void Logger::OnMessageOut(LOGGER_MSG& Msg)
+{
+    printf("%s",Msg.String.c_str());
+}
+
+bool Logger::ReadFirstMsg(LOGGER_MSG* pMsg)
+{
+    if(pMsg==NULL) return true;
+    if(MsgList.size()==0) return false;
+
+    std::list<LOGGER_MSG>::iterator it=MsgList.begin();
+    (*pMsg)=*it;
+    MsgList.pop_front();
+    return true;
+}
+
+bool Logger::ReadLastMsg(LOGGER_MSG* pMsg)
+{
+    if(pMsg==NULL) return true;
+    if(MsgList.size()==0) return false;
+
+    std::list<LOGGER_MSG>::iterator it=MsgList.end();
+    it--;
+    (*pMsg)=*it;
+    MsgList.pop_back();
+    return true;
+}
+
+void Logger::Message(const char* str,...)
+{
+    pthread_mutex_lock(&CS2);
+    va_list args;
+    va_start(args, str);
+    BaseOutV(LOGGER_MSG_TYPE_INFO,str,args);
+    va_end(args);
+    pthread_mutex_unlock(&CS2);
+}
+
+void Logger::Warning(const char* str,...)
+{
+    pthread_mutex_lock(&CS2);
+    va_list args;
+    va_start(args, str);
+    BaseOutV(LOGGER_MSG_TYPE_WARNING,str,args);
+    va_end(args);
+    pthread_mutex_unlock(&CS2);
+}
+
+void Logger::Error(const char* str,...)
+{
+    pthread_mutex_lock(&CS2);
+    va_list args;
+    va_start(args, str);
+    BaseOutV(LOGGER_MSG_TYPE_ERROR,str,args);
+    va_end(args);
+    pthread_mutex_unlock(&CS2);
+}
