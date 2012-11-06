937a455f3b0ed489a594976f760fca06e58cb1e7
Snowball <sashapolo239@gmail.com>
2012-11-06 15:59:00 +0400

    map is now parsed from the standard input

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <fstream>
 
 #include "Solver.h"
 #include "Logger.h"
@@ -20,23 +19,14 @@
  * @param string mapFileName - имя файла карты.
  * @return результат создания поля.
  */
-Field* createField(const std::string mapFileName) {
-	std::ifstream file;
+Field* createField(std::istream &file) {
 	char* file_buf;
 	int file_size;
-
-	file.open(mapFileName.c_str(), std::ifstream::in);
-
-	if(!file.is_open()) {
-		LOGERROR("Can't load map from \"%s\": can't open file", mapFileName.c_str());
-		return NULL;
-	};
 
 	file.seekg (0, std::ios::end);
 	file_size = file.tellg();
 	if(file_size==0) {
-		LOGERROR("Can't load map from \"%s\": file is empty", mapFileName.c_str());
-		file.close();
+		LOGERROR("Can't load map : file is empty");
 		return NULL;
 	}
 
@@ -46,18 +36,16 @@ Field* createField(const std::string mapFileName) {
 	if (file_buf[file_size -1] != '\n') {
 		file_buf[file_size] = '\n';
 	}
-	file.close();
 	Field *result;
 	try {
 		result = new Field(file_buf);
 	} catch (Field::FieldParseException&) {
-		LOGERROR("Can't load map from \"%s\": map is incorrect", mapFileName.c_str());
-		file.close();
+		LOGERROR("Can't load map: map is incorrect");
 		delete [] file_buf;
 		return NULL;
 	}
 	delete [] file_buf;
-	LOGINFO("Map loaded from \"%s\"", mapFileName.c_str());
+	LOGINFO("Map loaded");
 	return result;
 }
 
@@ -96,39 +84,34 @@ void drawStepByStep(Field* const pField, const std::string path) {
  * @return результат запускад.
  */
 int main(int argc, char** argv) {
-    if (argc != 2) {
-    	printf("Usage: solver <map path>\n");
-    	return 1;
-    }
-
 	HTMLLogger Logger;
     Logger.Init("LOG.html","MainLog");
     SetLogger(&Logger);
 
-    Field* field = createField(argv[1]);
+    Field* field = createField(std::cin);
 	if (!field) {
 		printf("Map load error! (See LOG.html)\n");
 		return 0;
 	}
-//	Solver s(field);
-//	std::string result = s.solve();
-//	std::cout<<result<<'\n';
+	Solver s(field);
+	std::string result = s.solve();
+	std::cout<<result<<'\n';
 //	drawStepByStep(field, result);
     
-    Field *oldField = field;
-	FieldSim fieldSim;
-	sSimResult res;
-	int nStep = 0;
-	char inStr[100];
-    printf("Controls:\n\tU - up\n\tD - down\n\tL - left\n\tR - right\n\tW - wait\n\tA - abort\nEnter - accept\n");
-    drawField(field, &res.path, nStep++);
-    while(true)
-    {
-        scanf("%s",inStr);
-        Field *newField = fieldSim.calcRobotSteps(oldField,inStr,&res);
-        drawField(newField,&res.path, nStep++);
-        printf("Score: %d, NumSteps: %d, NumLambdas: %d, LC: %d, State: %s\n",res.score,res.stepsTaken,res.lambdaReceived,newField->getLambdaCount(),stateToStr(res.state));
-        oldField = newField;
-    };
+//    Field *oldField = field;
+//	FieldSim fieldSim;
+//	sSimResult res;
+//	int nStep = 0;
+//	char inStr[100];
+//    printf("Controls:\n\tU - up\n\tD - down\n\tL - left\n\tR - right\n\tW - wait\n\tA - abort\nEnter - accept\n");
+//    drawField(field, &res.path, nStep++);
+//    while(true)
+//    {
+//        scanf("%s",inStr);
+//        Field *newField = fieldSim.calcRobotSteps(oldField,inStr,&res);
+//        drawField(newField,&res.path, nStep++);
+//        printf("Score: %d, NumSteps: %d, NumLambdas: %d, LC: %d, State: %s\n",res.score,res.stepsTaken,res.lambdaReceived,newField->getLambdaCount(),stateToStr(res.state));
+//        oldField = newField;
+//    };
     return 0;
 }
