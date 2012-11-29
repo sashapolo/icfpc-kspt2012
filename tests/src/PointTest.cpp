20cb8f459734e4c99adead60c5a3325dd55c20a1
Alexander <zigfive@mail.ru>
2012-11-30 00:25:05 +0400

    Тесты для операций вычитания

--- tests/src/PointTest.cpp
+++ tests/src/PointTest.cpp
@@ -20,10 +20,20 @@ void PointTest::testPlus() {
     testPlusRightResults();
 }
 
+void PointTest::testMinus() {
+    testMinusRightResults();
+}
+
 void PointTest::testPlusRightResults() {
     testPlusRightResultsNeg();
     testPlusRightResultsZero();
     testPlusRightResultsPos();
+}
+
+void PointTest::testMinusRightResults() {
+    testMinusRightResultsNeg();
+    testMinusRightResultsZero();
+    testMinusRightResultsPos();
 }
 
 void PointTest::testPlusRightResultsNeg() {
@@ -48,4 +58,28 @@ void PointTest::testPlusRightResultsPos() {
     Point p3 = p1 + p2;
     CPPUNIT_ASSERT(p3.x == 55);
     CPPUNIT_ASSERT(p3.y == 170);
+}
+
+void PointTest::testMinusRightResultsNeg() {
+    Point p1(-13, -42);
+    Point p2(-42, -128);
+    Point p3 = p1 - p2;
+    CPPUNIT_ASSERT(p3.x == 29);
+    CPPUNIT_ASSERT(p3.y == 86);
+}
+
+void PointTest::testMinusRightResultsZero() {
+    Point p1(0, 0);
+    Point p2(0, 0);
+    Point p3 = p1 - p2;
+    CPPUNIT_ASSERT(p3.x == 0);
+    CPPUNIT_ASSERT(p3.y == 0);
+}
+
+void PointTest::testMinusRightResultsPos() {
+    Point p1(13, 42);
+    Point p2(42, 128);
+    Point p3 = p1 - p2;
+    CPPUNIT_ASSERT(p3.x == -29);
+    CPPUNIT_ASSERT(p3.y == -86);
 }
